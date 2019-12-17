d2a69785c8cb55eb71fec9bdc75cdadecd231e85
rociop21 <rocio.pantalone@gmail.com>
2019-12-17 13:38:32 -0300

    Recipes Data edited

--- src/components/Recipes/RecipesData.js
+++ src/components/Recipes/RecipesData.js
@@ -1,25 +1,166 @@
 export const recipesData = {
     recipe1: {
         number: '1',
-        title: 'Mascarilla de mayonesa',
+        title: 'Anti-frizz mask',
         description: 'Aca va la descripcion',
         ingredients: [
-            '1 cup of mayonesa',
-            '1/2 cup of olive oil',
-            '3 huevo'        
+            '2 teaspoon of physiological solution',
+            '2 teaspoon of sugar',
+            '2 tablespoon of hydrating mask'       
         ],
-        instructions: 'Mezclamos todo en un bol y lo usamos en el cabello, haciendo especial énfasis en las puntas. Tras extenderlo, envolvemos nuestro pelo en una toalla y lo dejamos así durante 20 minutos. Después, aclaramos con agua fría.'
+        instructions: 'Wash you hair as usual, put the mask in the ends and work your way up (remember to avoid puting the mask in the scalp), wait 20 minutes, wash with warm water and use you usual conditioner.'
     },
     recipe2: {
         number: '2',
-        title: 'Mascarilla de huevo',
+        title: 'Mayonese mask',
         description: 'Aca va la descripcion',
         ingredients: [
-            '1/2 cup of olive oil',
-            '3 huevo'        
+            '2 tablespoon of mayonese',
+            '2 tablespoon of yogurt',
+            '1 teaspoon of olive oil',
+            '2 tablespoon of nutritive mask'       
         ],
-        instructions: 'Mezclamos todo en un bol y lo usamos en el cabello, haciendo especial énfasis en las puntas. Tras extenderlo, envolvemos nuestro pelo en una toalla y lo dejamos así durante 20 minutos. Después, aclaramos con agua fría.'
+        instructions: 'Wash you hair as usual, put the mask in the ends and work your way up (remember to avoid puting the mask in the scalp), wait 30 minutes with your shower cap, wash with warm water and use you usual conditioner.'
     },
+    recipe3: {
+        number: '3',
+        title: 'MHydrating shock',
+        description: 'Aca va la descripcion',
+        ingredients: [
+            '2 teaspoon of physiological solution',
+            '1 teaspoon of sugar',
+            '1 teaspoon of honey',
+            '3 tablespoon of hydrating mask'       
+        ],
+        instructions: 'Wash you hair as usual, put the mask in the ends and work your way up (remember to avoid puting the mask in the scalp), wait 35 minutes, wash with warm water and use you usual conditioner.'
+    },
+    recipe4: {
+        number: '4',
+        title: 'Cornstarch mask',
+        description: 'Aca va la descripcion',
+        ingredients: [
+            '2 teaspoon of physiological solution',
+            '1 tablespoon of cornstarch',
+            '2 tablespoon of hydrating mask'       
+        ],
+        instructions: 'Wash you hair as usual, put the mask in the ends and work your way up (remember to avoid puting the mask in the scalp), wait 30 minutes, wash with warm water and use you usual conditioner.'
+    },
+    recipe5: {
+        number: '5',
+        title: 'Reconstructive mask',
+        description: 'Aca va la descripcion',
+        ingredients: [
+            '1 tablespoon of unflavored gelatin',
+            '1 tablespoon of apple cider vinegar',
+            '2 tablespoon of hydrating mask'       
+        ],
+        instructions: 'Wash you hair as usual, put the mask in the ends and work your way up (remember to avoid puting the mask in the scalp), wait 30 minutes with your shower cap, wash with warm water and use you usual conditioner.'
+    },
+    recipe6: {
+        number: '6',
+        title: 'Acid mask',
+        description: 'Aca va la descripcion',
+        ingredients: [
+            '1 tablespoon of lemon juice',
+            '2 tablespoon of nutritive mask'       
+        ],
+        instructions: 'Wash you hair as usual, put the mask in the ends and work your way up (remember to avoid puting the mask in the scalp), wait 5-8 minutes, wash with warm water and use you usual conditioner.'
+    },
+    recipe7: {
+        number: '7',
+        title: 'Acid mask',
+        description: 'Aca va la descripcion',
+        ingredients: [
+            '2 tablespoon of apple cider vinegar',
+            '2 tablespoon of hydrating mask or conditioner'      
+        ],
+        instructions: 'Wash you hair as usual, put the mask in the ends and work your way up (remember to avoid puting the mask in the scalp), wait 5-8 minutes, wash with warm water and use you usual conditioner.'
+    },
+    recipe8: {
+        number: '8',
+        title: 'Coconut milk mask',
+        description: 'Aca va la descripcion',
+        ingredients: [
+            '1 tablespoon of coconut milk',
+            '1 tablespoon of olive oil',
+            '2 tablespoon of hydrating mask'        
+        ],
+        instructions: 'Wash you hair as usual, put the mask in the ends and work your way up (remember to avoid puting the mask in the scalp), wait 30 minutes, wash with warm water and use you usual conditioner.'
+    },
+    recipe9: {
+        number: '9',
+        title: 'Banana mask',
+        description: 'Aca va la descripcion',
+        ingredients: [
+            '1 banana',
+            '1 tablespoon of olive oil'       
+        ],
+        instructions: 'Blend together and strain. Wash you hair as usual, put the mask in the ends and work your way up (remember to avoid puting the mask in the scalp), wait 30 minutes, wash with warm water and use you usual conditioner.'
+    },
+    recipe10: {
+        number: '10',
+        title: 'Aloe Vera mask',
+        description: 'Aca va la descripcion',
+        ingredients: [
+            'Water',
+            'Aloe vera'           
+        ],
+        instructions: 'Cut the aloe vera leafs and blend. Mix equal parts of aloe vera and water. Wash you hair as usual, put the mask in the ends and work your way up (remember to avoid puting the mask in the scalp), wait 30 minutes, wash with warm water and use you usual conditioner.'
+    },
+    recipe11: {
+        number: '11',
+        title: 'Glycerine mask',
+        description: 'Aca va la descripcion',
+        ingredients: [
+            '1 tablespoon of glycerine',
+            '2 tablespoon of hydrating mask',
+
+            'optional:',
+            '2 teaspoon of physiological solution',
+            '1 teaspoon of sugar',
+            '1 teaspoon of honey'      
+        ],
+        instructions: 'Wash you hair as usual, put the mask in the ends and work your way up (remember to avoid puting the mask in the scalp), wait 30 minutes, wash with warm water and use you usual conditioner. Not recommended for very dry weathers or porous hair.'
+    },
+    recipe12: {
+        number: '12',
+        title: 'Constarch mask',
+        description: 'Aca va la descripcion',
+        ingredients: [
+            '1 tablespoon of cornstarch',
+            '1 cup of milk or water',
+            '1 tablespoon of hydrating mask'      
+        ],
+        instructions: 'Mix the cornstarch and water o milk, heat at low temperature until it disolves and thikens, let it cool until room temperature; finaly, add the mask and mix really well. Wash you hair as usual, put the mask in the ends and work your way up (remember to avoid puting the mask in the scalp), wait 0 minutes, wash with warm water and use you usual conditioner.'
+    },
+    recipe13: {
+        number: '13',
+        title: 'Mango mask',
+        description: 'Aca va la descripcion',
+        ingredients: [
+            '1 mango',
+            '2 teaspoon of olive oil',
+            '2 teaspoon of coconut oil',
+            '2 tablespoon of hydrating mask',
+
+            'optional:',
+            '1 rip banana'    
+        ],
+        instructions: 'Mix all in the blender and strain. Wash you hair as usual, put the mask in the ends and work your way up (remember to avoid puting the mask in the scalp), wait 0 minutes, wash with warm water and use you usual conditioner.'
+    },
+    recipe14: {
+        number: '14',
+        title: 'Banana mask',
+        description: 'Aca va la descripcion',
+        ingredients: [
+            '1 rip banana',
+            '1 egg',
+            '3 teaspoon of honey',
+            '5 teaspoon of olive oil'      
+        ],
+        instructions: 'Wash you hair as usual, put the mask in the ends and work your way up (remember to avoid puting the mask in the scalp), wait 0 minutes, wash with warm water and use you usual conditioner.'
+    },
+
 }
 
 export default recipesData;
